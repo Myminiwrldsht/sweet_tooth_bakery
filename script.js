6a121a4b0497649d639d2770d7e4e44629b5bd89
cyprianmbau <mbaucyprian@gmail.com>
2026-05-05 00:31:04 +0300

    add a special offer function.

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ function addStamp() {
 
     }
 }
+
+// daily special
+const special = document.getElementById("special");
+// 0=sunday 1=monday 5=friday 6=saturday
+let day = newDate().getDay();
+// switch statement
+let menu;
+switch (day) {
+    case 0:
+        menu = "Sunday Foam cakes";
+        break;
+    case 1:
+        menu = "Monday cholate cakes";
+        break;
+    case 5:
+        menu = "Friday fruit cake";
+        break;
+    case 6:
+        menu = "saturday butter cake";
+        break;
+    default:
+        menu = "no special offer today";
+
+}
+special.textContent = menu;
