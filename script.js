a9c4d53cd20c299ef0af43996da15488bb9f6b0f
cyprianmbau <mbaucyprian@gmail.com>
2026-05-04 23:42:24 +0300

    create the for loop for the stamps processing.

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let stamps = 0;
 const maxStamps = 10;
 const card = document.getElementById("cards");
 const message = document.getElementById("message");
+
+// the for loop
+for (let i = 0; i < maxStamps; 1++) {
+    let div = document.createElement("div");
+    div.classList.add("stamp");
+    card.appendChild(div);
+}
