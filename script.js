31e3b91b8747f35cf87c3aba1cd3a14c37594593
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-05 08:47:07 +0300

    feat: add a const shop status variable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const shopName ="Sweet Tooth Bakery";
+const shopName ="Sweet Tooth Bakery"; // creates a constant variable called ShopName.
 shopName = "Sweet Tooth Bakery";
 
 
+const shopStatus = isOpen ? "Fresh pastries available!" : "Sorry, we're closed!"; //creates a constant variable called shopStatus.
 
 // console.log(shopName);
