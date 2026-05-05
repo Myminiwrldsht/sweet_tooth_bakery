3b2ca5956dce0ff886497ff2e2544c68110e06fe
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-05 08:53:05 +0300

    chore: add menu items

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const shopName ="Sweet Tooth Bakery"; // creates a constant variable called ShopName.
 shopName = "Sweet Tooth Bakery";
-
-
 const shopStatus = isOpen ? "Fresh pastries available!" : "Sorry, we're closed!"; //creates a constant variable called shopStatus.
 
+
+const menu = ["bread", "cupcake", "croissant", "donut", "cake", "muffin"]; //creates an array of menu items
+menu: ["cread", "cupcake", "Croissant", "donut", "cake", "muffin"];
 // console.log(shopName);
