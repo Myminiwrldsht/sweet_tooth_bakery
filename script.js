8ae6f271ed65ae6e6d03378efae58a8dcec41ff6
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-05 14:06:10 +0300

    feat:built menu display logic

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const shopName ="Sweet Tooth Bakery"; // creates a constant variable called ShopName.
-shopName = "Sweet Tooth Bakery";
+let isOpen = true;
 const shopStatus = isOpen ? "Fresh pastries available!" : "Sorry, we're closed!"; //creates a constant variable called shopStatus.
 
 
 const menu = ["bread", "cupcake", "croissant", "donut", "cake", "muffin"]; //creates an array of menu items
-menu: ["cread", "cupcake", "Croissant", "donut", "cake", "muffin"];
 
 
-const formatCurrency = amount => `Ksh${amount.toFixed(2)}`;
+const formatCurrency = amount => `Ksh ${amount.toFixed(2)}`;
+
+//menu display logic
+//print headings function
+function displayMenu () {
+console.log ("\n ---WELCOME TO SWEET TOOTH BAKERY---");
+console.log("\n --FRESHLY BAKED DAILY--");
+console.log("\n -MENU-");
+
+let menuDisplay = {}; //stores menu items and prices
+
+
+for (let i = 0; i < menu.length; i++){
+    const item = menu[i];
+    const price = getItemPrice(item);
+
+    menuDisplay[item] = price;
+    console.log (`\n ${i + 1}. ${item} -${formatCurrency(price)}`);
+}
+
+return menuDisplay;
+
+} 
 // console.log(shopName);
